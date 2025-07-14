e0e66ebc97f0ab5bfffcbca8d777a49a15be07ce
Rafael <rafaelfigueiredo14nunes@gmail.com>
2025-07-14 16:56:25 -0300

    Rotas para admin

--- MyVaccine/routes/web.php
+++ MyVaccine/routes/web.php
@@ -9,6 +9,21 @@ use App\Http\Controllers\Vaccines\StockController;
 use App\Http\Controllers\Vaccines\VaccinationHistoryController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Admin\AdminAuthController;
+
+
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    Route::post('logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+    Route::get('home', [AdminAuthController::class, 'showLoginForm'])->name('admin.home');
+});
+
+
+
 
 // Página inicial pública
 Route::get('/', function () {
