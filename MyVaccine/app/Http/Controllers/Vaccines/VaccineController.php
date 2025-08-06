9415cae6dc24f7e69dd44a9a5c58fe4387006aa8
HatusLuiz <144400435+HatusLuiz@users.noreply.github.com>
2025-08-06 10:05:42 -0300

    Ajustes do VaccineController (#15)
    Ajustes no Vaccine Controller

--- MyVaccine/app/Http/Controllers/Vaccines/VaccineController.php
+++ MyVaccine/app/Http/Controllers/Vaccines/VaccineController.php
@@ -6,35 +6,44 @@ use App\Http\Controllers\Controller;
 use App\Models\Vaccine;
 use Illuminate\Http\Request;
 
+
 class VaccineController extends Controller
 {
     public function index()
     {
         $vaccines = Vaccine::all();
-        return view('vaccine.index', compact('vaccines'));
+        return view('admin.home', compact('vaccines'));
     }
+
 
     public function create()
     {
         return view('vaccine.create');
     }
 
+
     public function store(Request $request)
     {
         $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
+            'min_age' => 'required|integer',
+            'max_age' => 'nullable|integer',
+            'contraindications' => 'nullable|string',
         ]);
 
+
         Vaccine::create($request->all());
-        return redirect()->route('vaccine.index')->with('success', 'Vacina adicionada com sucesso!');
+        return redirect()->route('vaccines.index')->with('success', 'Vacina adicionada com sucesso!');
     }
+
 
     public function edit($id)
     {
         $vaccine = Vaccine::findOrFail($id);
         return view('vaccine.edit', compact('vaccine'));
     }
+
 
     public function update(Request $request, $id)
     {
@@ -43,9 +52,10 @@ class VaccineController extends Controller
         return redirect()->route('vaccine.index')->with('success', 'Vacina atualizada com sucesso!');
     }
 
+
     public function destroy($id)
     {
         Vaccine::destroy($id);
-        return redirect()->route('vaccine.index')->with('success', 'Vacina removida.');
+        return redirect()->route('vaccines.index')->with('success', 'Vacina removida.');
     }
 }
